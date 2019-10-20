0f59a20711194db4669273046b7d6db34bebba9b
Sebiann <redstonetriforce@gmail.com>
2019-10-20 11:38:04 +0200

    Leaderboard works (hopefully)
    Signed-off-by: Sebiann <redstonetriforce@gmail.com>

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js')
+
 module.exports = {
   name: 'leaderboard',
   aliases: ['lb'],
@@ -8,16 +10,42 @@ module.exports = {
   args: true,
   cooldown: 5,
   execute(message, args) {
-    if (!args[1] === '') {
+    let leaderboard
+
+    if (args[1] !== '') {
       if (args[0] === 'elytraone') {
-        let leaderboard = message.client.getElytraOne.get(args[1])
-        message.channel.send(`${leaderboard}`)
+        leaderboard = message.client.getElytraOne.get(args[1])
+        const leaderEmbed = new Discord.RichEmbed()
+          .setColor('#FF0000')
+          .setTitle('Leaderboard:')
+          .setURL(leaderboard.proof)
+          .setDescription('Some Leaderboard')
+          .addField('Username:', leaderboard.user)
+          .addBlankField()
+          .addField('Rank:', leaderboard.rank, true)
+          .addField('Time:', leaderboard.time, true)
+          .setTimestamp()
+
+        console.log(leaderboard)
+        message.channel.send(leaderEmbed)
           .then(msg => {
             msg.delete(5000)
           })
       } else if (args[0] === 'funhouseone') {
-        let leaderboard = message.client.getFunhouseOne.get(args[1])
-        message.channel.send(`${leaderboard}`)
+        leaderboard = message.client.getFunhouseOne.get(args[1])
+        const leaderEmbed = new Discord.RichEmbed()
+          .setColor('#FF0000')
+          .setTitle('Leaderboard:')
+          .setURL(leaderboard.proof)
+          .setDescription('Some Leaderboard')
+          .addField('Username:', leaderboard.user)
+          .addBlankField()
+          .addField('Rank:', leaderboard.rank, true)
+          .addField('Time:', leaderboard.time, true)
+          .setTimestamp()
+
+        console.log(leaderboard)
+        message.channel.send(leaderEmbed)
           .then(msg => {
             msg.delete(5000)
           })
--- commands/settime.js
+++ commands/settime.js
@@ -8,14 +8,23 @@ module.exports = {
   args: true,
   cooldown: 5,
   execute(message, args) {
-    let time = { user: args[1], rank: 0, time: args[2], proof: '' }
+    let uservar = args[1]
+    let timevar = args[2]
     if (args[0] === 'elytraone') {
+      let time = message.client.getElytraOne.get(uservar)
+      if (!time) {
+        time = { user: uservar, rank: 0, time: timevar, proof: '' }
+      }
       message.client.setElytraOne.run(time)
       message.channel.send(`Your Time was ${time.time}`)
         .then(msg => {
           msg.delete(5000)
         })
     } else if (args[0] === 'funhouseone') {
+      let time = message.client.getFunhouseOne.get(uservar)
+      if (!time) {
+        time = { user: uservar, rank: 0, time: timevar, proof: '' }
+      }
       message.client.setFunhouseOne.run(time)
       message.channel.send(`Your Time was ${time.time}`)
         .then(msg => {
